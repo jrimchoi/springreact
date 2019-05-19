b135879f562ec51762fb469838c42c0f53185e92
jrimchoi <jrimchoi@naver.com>
2019-05-20 02:01:44 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Navigation } from "react-router";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Grid, GridColumn } from "@progress/kendo-react-grid";
+import "@progress/kendo-theme-default/dist/all.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("/contacts").then(res => {
+      this.setState({ contacts: res.data });
+      console.log(this.state.contacts);
+    });
+  }
+  OnRowSelected(sender, eventArgs) {
+    var grid = sender;
+    var MasterTable = grid.get_masterTableView();
+    var row = MasterTable.get_dataItems()[
+      eventArgs.get_itemIndexHierarchical()
+    ];
+    var cell = MasterTable.getCellByColumnUniqueName(row, "ContactName");
+    alert(cell.innerHTML); //here cell.innerHTML holds the value of the cell
+  }
+  rowClick = event => {
+    console.log(event.dataItem.id);
+    //event.preventDefault();
+    // this.transitionTo("/show/${event.dataItem.id}");
+    // this.context.router.transitionTo("/show/${event.dataItem.id}");
+    this.props.history.push("/show/" + event.dataItem.id);
+  };
+  render() {
+    return (
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">CONTACTS LIST</h3>
+          </div>
+          <div class="panel-body">
+            <h4>
+              <Link to="/create">
+                <span
+                  class="glyphicon glyphicon-plus-sign"
+                  aria-hidden="true"
+                />{" "}
+                Add Contact
+              </Link>
+            </h4>
+            <Grid data={this.state.contacts} onRowClick={this.rowClick}>
+              <GridColumn field="name" title="Name" />
+              <GridColumn field="address" />
+            </Grid>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
